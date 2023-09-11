3585bdacf3eb4ac3b9fc55bdcd69453c0828bc64
Javid Babaev <dzh.babaev@gmail.com>
2023-09-12 01:01:59 +0300

    add migration

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,27 @@ import (
 	"github.com/MrClean-code/dayling/pkg/handler"
 	"github.com/MrClean-code/dayling/pkg/repository"
 	"github.com/MrClean-code/dayling/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(dayling.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatal("error occured white running http server: %s", err.Error())
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured white running http server: %s", err.Error())
 	}
 
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
